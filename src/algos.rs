6930a8f9ff46c00a6137c0b5a35264cd3980ae3c
ltlollo <ltlollo.it@gmail.com>
2014-07-18 13:15:41 +0200

    separate rust version

---
+++ src/algos.rs
@@ -0,0 +1,231 @@
+#![crate_id = "http://github.com/ltlollo/string-matcher#algos:0.1.0"]
+#![crate_type = "lib"]
+#![desc = "Some algoritms"]
+#![license = "GPLv2"]
+#![warn(non_camel_case_types)]
+
+pub fn match_cont<T: PartialEq>(fst: &[T], snd: &[T]) -> uint {
+    let mut mch_max :uint = 0;
+    let mut mch: uint;
+    let mut it = fst.iter();
+
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(snd.iter()) {
+            if *f == *s {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None {
+            break;
+        }
+    }
+    it = snd.iter();
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(fst.iter()) {
+            if *f == *s {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None {
+            break;
+        }
+    }
+    mch_max
+}
+
+pub fn match_cont_sim<T: PartialEq>(fst: &[T], snd: &[T]) -> uint {
+    let mut mch_max :uint = 0;
+    let mut mch: uint;
+    let mut it = fst.iter();
+    let mut len = fst.len();
+
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(snd.iter()) {
+            if *f == *s {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None || mch_max >= len {
+            break;
+        }
+        len-=1;
+    }
+    it = snd.iter();
+    len = snd.len();
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(fst.iter()) {
+            if *f == *s {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None || mch_max >= len {
+            break;
+        }
+        len-=1;
+    }
+
+    mch_max
+}
+
+/*
+pub fn match_cont_sim ... {
+    ...
+    loop {
+        let mch: uint = it.zip(snd.iter()).fold(0, |accumulator, item| {
+            let (f, s) = item; 
+            if *f == *s {
+                accumulator+1 
+            } else {
+                accumulator
+            }
+        });
+        ...
+    }
+    it = snd.iter();
+    len = snd.len();
+    loop {
+        let mch: uint = it.zip(fst.iter()).fold(0, |accumulator, item| {
+            let (f, s) = item; 
+            if *f == *s {
+                accumulator+1 
+            } else {
+                accumulator
+            }
+        });
+        ...
+    }
+    mch_max
+}
+*/
+
+pub fn match_cont_sim_trim<T: PartialEq>(fst: &[T], snd: &[T], fun: |f: &T, s: &T| -> bool) -> uint {
+    let mut mch_max: uint = 0;
+    let mut mch: uint;
+    let mut it = fst.iter();
+    let mut len = fst.len();
+
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(snd.iter()) {
+            if fun(f, s) {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None || mch_max >= len {
+            break;
+        }
+        len-=1;
+    }
+    it = snd.iter();
+    len = snd.len();
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(fst.iter()) {
+            if fun(f, s) {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None || mch_max >= len {
+            break;
+        }
+        len-=1;
+    }
+
+    mch_max
+}
+
+pub fn match_cont_trim<T: PartialEq>(fst: &[T], snd: &[T], fun: |f: &T, s: &T| -> bool) -> uint {
+    let mut mch_max :uint = 0;
+    let mut mch: uint;
+    let mut it = fst.iter();
+
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(snd.iter()) {
+            if fun(f, s) {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None {
+            break;
+        }
+    }
+    it = snd.iter();
+    loop {
+        mch = 0;
+        for (f, s) in it.zip(fst.iter()) {
+            if fun(f, s) {
+                mch+=1;
+            }
+        }
+        if mch > mch_max {
+            mch_max = mch;
+        }
+        if it.next() == None {
+            break;
+        }
+    }
+    mch_max
+}
+
+pub fn match_norm<T: PartialEq>(fst: &[T], snd: &[T]) -> f64 {
+    let len = if fst.len() > snd.len() { fst.len() } else { snd.len() };
+    if len != 0 {
+        (match_norm(fst, snd) as f64)/(len as f64)
+    } else {
+        0.0
+    }
+}
+
+pub fn match_norm_sim<T: PartialEq>(fst: &[T], snd: &[T]) -> f64 {
+    let len = if fst.len() > snd.len() { fst.len() } else { snd.len() };
+    if len != 0 {
+        (match_cont_sim(fst, snd) as f64)/(len as f64)
+    } else {
+        0.0
+    }
+}
+
+pub fn match_norm_trim<T: PartialEq>(fst: &[T], snd: &[T], fun: |f: &T, s: &T| -> bool) -> f64 {
+    let len = if fst.len() > snd.len() { fst.len() } else { snd.len() };
+    if len != 0 {
+        (match_cont_trim(fst, snd, fun) as f64)/(len as f64)
+    } else {
+        0.0
+    }
+}
+
+pub fn match_norm_sim_trim<T: PartialEq>(fst: &[T], snd: &[T], fun: |f: &T, s: &T| -> bool) -> f64 {
+    let len = if fst.len() > snd.len() { fst.len() } else { snd.len() };
+    if len != 0 {
+        (match_cont_sim_trim(fst, snd, fun) as f64)/(len as f64)
+    } else {
+        0.0
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+#![crate_id = "http://github.com/ltlollo/string-matcher#string-matcher:0.0.1"]
+#![desc = "Find similar files"]
+#![license = "GPLv2"]
+#![warn(non_camel_case_types)]
+
+extern crate algos;
+
+use std::io::fs::readdir;
+use std::comm::{channel, Sender, Receiver};
+use algos::match_norm_sim;
+
+static ntasks: uint = 4;
+
+type Data = (String, String);
+
+struct Res {
+    pair: Data,
+    mch: f64
+}
+
+fn worker(rx: &Receiver<Data>, num: uint, tx: Sender<Res>) {
+    for _ in range(0, num) {
+        let (f, s) = rx.recv();
+        let m = match_norm_sim(f.as_bytes(), s.as_bytes());
+        tx.send(Res { pair: (f, s), mch: m });
+    }
+}
+
+fn work_balancer(data: &Vec<Path>, tx: &Sender<Res>) {
+    let total_load: uint = (data.len()*(data.len()-1))/2;
+    let thread_load: uint = total_load/ntasks;
+
+    let mut tchs: Vec<Sender<Data>> = Vec::with_capacity(ntasks);
+    for id in range(0, ntasks) {
+        let (tx_d, rx_d): (Sender<Data>, Receiver<Data>) = channel();
+        tchs.push(tx_d);
+        let rest_load = if total_load % ntasks != 0 && id < total_load % ntasks {
+            1 } else { 0
+        };
+        let tx_r = tx.clone();
+        spawn(proc() {
+            worker(&rx_d, thread_load + rest_load, tx_r);
+        });
+    }
+
+    let mut m: uint = 0;
+    for i in range(0, data.len()-1) {
+        for j in range(i+1, data.len()) {
+            let f = String::from_str(data.get(i).as_str().unwrap());
+            let s = String::from_str(data.get(j).as_str().unwrap());
+            tchs.get(m).send((f, s));
+            m=(m+1)%ntasks;
+        }
+    }
+}
+
+fn find_similar(data: &Vec<Path>) {
+    if data.len() < 2 {
+        return;
+    }
+
+    let (tx, rx): (Sender<Res>, Receiver<Res>) = channel();
+
+    work_balancer(data, &tx);
+
+    let load: uint = data.len()*(data.len()-1)/2;
+    let mut res: Vec<Res> = Vec::with_capacity(load);
+    for _ in range(0, load) {
+        res.push(rx.recv());
+    }
+    res.sort_by(|f: &Res, s: &Res| -> Ordering
+        if f.mch > s.mch { Greater } else if f.mch < s.mch { Less } else {
+            Equal
+        }
+    );
+    for it in res.iter().rev() {
+        let (ref f, ref s) = it.pair;
+        println!("{}, {} is {}", f, s, it.mch);
+    }
+}
+
+fn main() {
+    let files = readdir(&Path::new("."));
+    match files {
+        Err(_) => (),
+        Ok(ref files) => find_similar(files),
+    }
+}
